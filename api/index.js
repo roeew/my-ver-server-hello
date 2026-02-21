0c1c54746bab83f562c0ab2906f07c0ca9884442
Roee Weisbert <roee111@gmail.com>
2026-02-21 17:24:54 +0200

    first

---
+++ api/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+let messages = [];
+
+// GET
+app.get("/allmessages", (req, res) => {
+    res.json(messages);
+});
+
+// GET with query parameter handling
+app.get("/message", (req, res) => {
+    const num = parseInt(req.query.num);
+    res.json(messages[num]);
+});
+
+// POST
+app.post("/messages", (req, res) => {
+    const message = req.body.text;
+    messages.push({ text: message });
+    res.json({ status: "ok" });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
